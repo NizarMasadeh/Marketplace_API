9a2bf7d61e010c945c7a38873255478a30f3a051
Nizar Masadeh <n.masadeh@futeric.com>
2024-12-28 20:44:16 +0300

    cors n stuff

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
 const helmet = require('helmet');
 const fileUpload = require('express-fileupload');
+require('dotenv').config();
+
 const userRoutes = require('./routes/users');
 const authRoutes = require('./routes/auth');
 const productRoutes = require('./routes/products');
@@ -12,22 +13,17 @@ const dataRoutes = require('./routes/data');
 const merchantRoutes = require('./routes/merchant');
 const insertUserData = require('./routes/nzrmUsers');
 const ipLocationRoutes = require('./routes/ipLocation');
-const { setSocketInstance } = require('./controllers/productController')
-const { setMerchantSocket } = require('./controllers/merchantController')
 const usersRoute = require('./routes/usersRoute');
 const lamoorRoutes = require('./routes/nzd-routes/lamoor');
-require('dotenv').config();
 const Pusher = require('pusher');
-const http = require('http');
-const server = http.createServer(app);
-const { Server } = require("socket.io");
-const io = new Server(server, {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
-    credentials: true,
-  },
-});
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
+app.use(fileUpload());
+
+
 
 const pusher = new Pusher({
   appId: process.env.PUSHER_APPID,
@@ -42,24 +38,7 @@ pusher.trigger('my-channel', 'my-event', { message: 'hello world' })
     console.error('Pusher Error:', error);
   });
   
-io.on("connection", (socket) => {
-  console.log('Connected socket: ', socket.id);
-  socket.emit('connection', { message: 'Welcome to the server!' });
-})
-setSocketInstance(io);
-setMerchantSocket(io);
 
-
-delete require.cache[require.resolve('./routes/merchant')];
-delete require.cache[require.resolve('./controllers/merchantController')];
-
-
-// In your server.js
-app.set('trust proxy', true);
-app.use(cors());
-app.use(helmet());
-app.use(express.json());
-app.use(fileUpload());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/products', productRoutes);
@@ -71,10 +50,21 @@ app.use('/api/merchants', merchantRoutes);
 app.use('/api/ipLocation', ipLocationRoutes);
 app.use('/api/nzrm-users', insertUserData);
 app.use('/api/ip-stuff', usersRoute);
-
 app.use('/api/lamoor', lamoorRoutes);
-const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
 });
+
+// For local development
+if (process.env.NODE_ENV !== 'production') {
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+module.exports = app;
+
